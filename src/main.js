89abc3111b937fd2aa10ec3fa5724d9f4c86d0ed
Polarbear08 <pistolstar0808@gmail.com>
2019-12-20 21:15:02 +0900

    added tag restriction

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 const sanitizeHtml = require('sanitize-html')
 
 const dirty = 'some really tacky HTML'
-const clean = sanitizeHtml(dirty)
+let clean = sanitizeHtml(dirty)
 
 console.log(dirty)
+console.log(clean)
+
+// Allow only a super restricted set of tags and attributes
+clean = sanitizeHtml(dirty, {
+    allowedTags: ['b', 'i', 'em', 'strong', 'a'],
+    allowedAttributes: {
+        'a': ['href']
+    },
+    allowedIframeHostnames: ['www.youtube.com']
+})
+
 console.log(clean)
